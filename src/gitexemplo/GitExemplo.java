7b6d43487e51a2565964f17b15029a13a1ce45f4
Natália Maia <nataliaborgesm@gmail.com>
2024-11-06 15:07:22 -0300

    Hello Word

--- src/gitexemplo/GitExemplo.java
+++ src/gitexemplo/GitExemplo.java
@@ -4,17 +4,10 @@
  */
 package gitexemplo;
 
-/**
- *
- * @author manue
- */
 public class GitExemplo {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println ("Hello Word!");
     }
     
 }
