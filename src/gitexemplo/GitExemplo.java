7b1d2a0db6bcb7180010c4ebd4fd9a884f02b54a
nataliamaia <nataliaborgesm@gmail.com>
2024-11-07 11:16:19 -0300

    Hello a todos

--- src/gitexemplo/GitExemplo.java
+++ src/gitexemplo/GitExemplo.java
@@ -7,7 +7,7 @@ package gitexemplo;
 public class GitExemplo {
 
     public static void main(String[] args) {
-        System.out.println ("Hello Word!");
+        System.out.println ("Hello a todos!");
     }
     
 }
